fd0483fa2837f3c8a5a6d51d59725579a98f6d5b
JuanMa <juanma.garrido@gmail.com>
2018-08-01 12:58:13 +0200

    Resize observer (#2)
    * MediaQuery custom w/ viewport
    
    * version w/ viewport

--- src/index.js
+++ src/index.js
@@ -1,31 +1,66 @@
-import React from 'react';
-import {ContainerQuery} from 'react-container-query';
+import React, { Component } from 'react';
+import ReactDOM from "react-dom"
+import ResizeObserver from 'resize-observer-polyfill';  
+import shallowEqual from 'shallowequal'
 
-const BREAKPOINTS = {
-  xs: '480',
-  s: '600',
-  m: '840',
-  l: '960',
-  xl: '1280',
-  xxl: '1440'
-}
+import {getWidth, matchQueries} from './helpers'
 
-const getQueryObject = BREAKPOINTS => 
-  Object.entries(BREAKPOINTS).reduce( (query, [size, breakpoint]) => {
-    query[size.toUpperCase()] = {
-      minWidth: breakpoint
+import BREAKPOINTS from './breakpoints.json'
+
+const MediaQueryFactory = BREAKPOINTS => 
+  class MediaQuery extends Component {
+    state = {
+      params: {}
     }
-    return query
-  },{})
+    containerResizeObserver = null
+    matchQueries = matchQueries(BREAKPOINTS)
 
-const MediaQueryFactory = BREAKPOINTS => props => {
-  const query = getQueryObject(BREAKPOINTS)
-  return (
-    <ContainerQuery query={query}>
-      { props.children }
-    </ContainerQuery>
-  )
-}
+    componentDidMount() {
+      const { viewport } = this.props
+      const container = ReactDOM.findDOMNode(this)
+      let initialWidth = 0
+      
+      if (viewport) {
+        window.addEventListener('resize', this.handleWindowResize)
+        initialWidth = window.outerWidth
+      }
+      else {
+        this.containerResizeObserver = new ResizeObserver(this.handleContainerResize)
+        this.containerResizeObserver.observe(container);
+        initialWidth = getWidth(container)
+      }
+
+      const result = matchQueries(initialWidth)
+      this.setState({ params: result })
+      
+    }
+
+    componentWillUnmount() {
+      this.containerResizeObserver.disconnect();
+      this.containerResizeObserver = null;
+      window.removeEventListener('resize', this.handleResize)
+    }
+
+    handleWindowResize = e => {
+      const { outerWidth: width } = e.target
+      const { params } = this.state
+      const result = this.matchQueries(width);
+      if (!shallowEqual(result, params)) this.setState({ params: result })
+    }
+
+    handleContainerResize = entries => {
+      const target = entries[0].target
+      const width = getWidth(target)
+      const { params } = this.state
+      const result = this.matchQueries(width)
+      if (!shallowEqual(result, params)) this.setState({ params: result })
+    }
+
+    render() {
+      return this.props.children(this.state.params);
+    }
+
+  }
 
 const MediaQuery = MediaQueryFactory(BREAKPOINTS)
 
